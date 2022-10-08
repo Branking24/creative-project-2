5b7da246d369917a2d84465963f5218bccbedfe9
Branden Kinghorn <brando3034king@gmail.com>
2022-10-08 19:45:05 +0000

    Adding full javascript functionality

--- script.js
+++ script.js
@@ -1,26 +1,40 @@
 let foxEndpoint = "https://randomfox.ca/floof/";
 let dogEndpoint = "https://random.dog/woof.json";
 
-function doResult(urlToFetch) {
+function doResult(urlToFetch, type) {
     fetch(urlToFetch)
         .then(function(response) {
             return response.json();
         })
         .then(function(json) {
             console.log(json);
-            if (json.url.includes(".mp4")) {
-                doResult(urlToFetch);
+            let val = "";
+            if (type === "dog") {
+                val = json.url;
+            }
+            else if (type === "fox") {
+                val = json.image;
+            }
+            if (type === "dog" && (json.url.includes(".mp4") || json.url.includes(".webm"))) {
+                doResult(urlToFetch, type);
             }
             else {
-            let result = "<img class=\"fox-image\" src=\"" + json.url + "\"/>";
-            result = result + "<form><input id=\"resetSubmit\" type=\"submit\" value=\"Reset\"></input></form>"
-            document.getElementById("img-result").innerHTML = result;
+                let result = "<img class=\"result-image\" src=\"" + val + "\"/>";
+                result = result + "<form><input id=\"resetSubmit\" type=\"submit\" value=\"Reset\"></input></form>"
+                document.getElementById("img-result").innerHTML = result;
             }
     });
 }
 
 document.getElementById("codeSubmit").addEventListener("click", function(event) {
     event.preventDefault();
+    let foxButton = document.getElementById("fox");
+    let dogButton = document.getElementById("dog");
+    if (foxButton.checked) {
+        doResult(foxEndpoint, "fox");
+    }
+    else if (dogButton.checked) {
+        doResult(dogEndpoint, "dog");
+    }
     
-    doResult(dogEndpoint);
 });
